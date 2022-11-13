74e2f279266a5d19befd2127879f8490e1226934
Hari Shekhar <shekhardtu@gmail.com>
2022-11-13 10:33:19 +0530

    feat: added payment slip

---
+++ src/components/PayslipTemplate.tsx
@@ -0,0 +1,282 @@
+import { useRouter } from 'next/router';
+import type { CSSProperties } from 'react';
+
+type Iwelcome = {
+  applicantName: string;
+  totalAmount: string;
+  balanceAmount: string;
+  paidAmount: string;
+  amountInWords: string;
+  caseFile: string;
+  paymentMode: string;
+  invoiceId: string;
+};
+const WelcomeTemplate = (props: Iwelcome) => {
+  const router = useRouter();
+  const styles = {
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignContent: 'center',
+      position: 'relative',
+      width: '590px',
+      margin: '20px 20px',
+      marginTop: 0,
+      marginBottom: '0',
+      height: '652px',
+    } as CSSProperties,
+    contentContainer: {
+      fontSize: '12px',
+
+      margin: '20px',
+      width: '94%',
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    } as CSSProperties,
+    contentCard: {
+      zIndex: 1,
+      backgroundColor: 'black',
+    } as CSSProperties,
+    title: {
+      fontFamily: 'Anton, Anton-Regular',
+      fontSize: '1rem',
+      color: 'white',
+      textAlign: 'center',
+    } as CSSProperties,
+    imgContainer: {
+      width: '300px',
+    } as CSSProperties,
+    img: {
+      display: 'block',
+      width: '100%',
+      height: '100%',
+    } as CSSProperties,
+    h1Heading: {
+      color: '#5145cc',
+      fontSize: '20px',
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+      padding: '15px',
+      paddingTop: '0',
+      width: '446px',
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    } as CSSProperties,
+
+    text1Box: {
+      display: 'flex',
+      color: '#000',
+      backgroundColor: '#fff',
+      fontWeight: 'bold',
+      justifyContent: 'space-between',
+      marginBottom: '10px',
+      width: '100%',
+      fontSize: '15px',
+      padding: '15px',
+    } as CSSProperties,
+
+    text1: {
+      display: 'inline-block',
+      width: '240px',
+      fontWeight: 'bold',
+    } as CSSProperties,
+
+    text2: {
+      display: 'inline-block',
+      width: '240px',
+      fontWeight: '400',
+      textAlign: 'right',
+    } as CSSProperties,
+
+    formBox: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      width: '100%',
+      backgroundColor: '#5145cc',
+      color: '#fff',
+      padding: '15px',
+      fontSize: '15px',
+      marginBottom: '20px',
+    } as CSSProperties,
+
+    formData: {
+      display: 'flex',
+      flexDirection: 'column',
+    } as CSSProperties,
+    formLabel: {
+      display: 'inline-block',
+      width: '140px',
+      fontWeight: 'bold',
+    } as CSSProperties,
+    formValue: {
+      display: 'inline-block',
+    } as CSSProperties,
+    formImgBox: {
+      display: 'flex',
+    } as CSSProperties,
+    formImg: {
+      width: '100px',
+      height: 'auto',
+      padding: '0',
+    } as CSSProperties,
+
+    text2Box: {
+      display: 'flex',
+      flexDirection: 'column',
+      fontSize: '16px',
+      width: '100%',
+      marginBottom: '20px',
+    } as CSSProperties,
+    text2Title: {
+      display: 'flex',
+      fontSize: '16px',
+      fontWeight: 'bold',
+      color: '#5145cc',
+      width: '100%',
+    } as CSSProperties,
+
+    text2Desc: {
+      display: 'flex',
+      fontSize: '14px',
+    } as CSSProperties,
+
+    footerBox: {
+      display: 'flex',
+      fontSize: '12px',
+      padding: '20px',
+      paddingTop: '0',
+      justifyContent: 'space-between',
+    } as CSSProperties,
+    footerText: {
+      display: 'flex',
+      flexDirection: 'column',
+    } as CSSProperties,
+    footerImg: {
+      display: 'block',
+      height: '80px',
+      padding: '5px',
+    } as CSSProperties,
+    footerSignature: {
+      marginTop: '10px',
+    } as CSSProperties,
+    box: {
+      display: 'flex',
+      flexDirection: 'column',
+    } as CSSProperties,
+    headerImg: {
+      display: 'flex',
+      width: '100%',
+      height: 'auto',
+      padding: '0',
+    } as CSSProperties,
+    mainBox: {
+      margin: '20px 0',
+    } as CSSProperties,
+    normalText: {
+      fontWeight: 400,
+    } as CSSProperties,
+  };
+  function getCurrentDate(separator = '/') {
+    const newDate = new Date();
+    const date = newDate.getDate();
+    const month = newDate.getMonth() + 1;
+    const year = newDate.getFullYear();
+
+    return `${date}${separator}${
+      month < 10 ? `0${month}` : `${month}`
+    }${separator}${year}`;
+  }
+  return (
+    <section className="relative flex flex-col justify-center m-auto w-full">
+      {/* <div style={styles.coverBg}></div> */}
+
+      {/* <img alt="" src={`${router.basePath}/assets/images/banner.jpg`} /> */}
+      <div style={styles.container}>
+        <div style={styles.box}>
+          <div style={styles.contentContainer}>
+            <div style={styles.h1Heading}> Payment Invoice</div>
+
+            <div style={styles.text1Box}>
+              <div style={styles.text1}>
+                <div> Bill To</div>
+                <div style={styles.normalText}> {props.applicantName} </div>
+              </div>
+              <div style={styles.text2}>
+                <div style={styles.text1}>
+                  <div>
+                    {' '}
+                    Invoice Id:{' '}
+                    <span style={styles.normalText}>
+                      {' '}
+                      {props.invoiceId}
+                    </span>{' '}
+                  </div>
+                </div>
+                <div style={styles.text1}>
+                  <div>
+                    {' '}
+                    Invoice Date:{' '}
+                    <span style={styles.normalText}>
+                      {getCurrentDate()}
+                    </span>{' '}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div style={styles.formBox}>
+              <div style={styles.formData}>
+                <div style={styles.formLabel}> Total Amount </div>
+                <div style={styles.formValue}> {props.totalAmount}</div>
+              </div>
+              <div style={styles.formData}>
+                <div style={styles.formLabel}> Received Amount</div>
+                <div style={styles.formValue}>{props.paidAmount}</div>
+              </div>
+              <div style={styles.formData}>
+                <div style={styles.formLabel}> Balance Amount</div>
+                <div style={styles.formValue}>{props.balanceAmount}</div>
+              </div>
+            </div>
+
+            <div style={styles.text2Box}>
+              <div style={styles.text2Title}>Amount In Words</div>
+
+              <span style={styles.text2Desc}>{props.amountInWords}</span>
+            </div>
+            <div style={styles.text2Box}>
+              <div style={styles.text2Title}>Payment Mode</div>
+
+              <span style={styles.text2Desc}>{props.paymentMode}</span>
+            </div>
+            <div style={styles.text2Box}>
+              <div style={styles.text2Title}>Description</div>
+
+              <span style={styles.text2Desc}>
+                Dear customer, we have received payment of Rs.{' '}
+                {props.paidAmount} processing fee for your case file no{' '}
+                {props.caseFile}
+              </span>
+            </div>
+          </div>
+          <div className="footer" style={styles.footerBox}>
+            <div style={styles.footerText}></div>
+            <div>
+              <img
+                style={styles.footerImg}
+                src={`${router.basePath}/assets/images/stamp.png`}
+                alt=""
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WelcomeTemplate;
--- src/pages/payslip.tsx
+++ src/pages/payslip.tsx
@@ -1,182 +1,228 @@
-import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 import { useRouter } from 'next/router';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
+import PayslipTemplate from '@/components/PayslipTemplate';
 import { Main } from '@/templates/Main';
 
 const Contact = () => {
-  const printRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const printRef = useRef<any>(null);
 
-  const [firstName, setFirstName]: [any, any] = useState('Raju');
-  const [lastName, setLastName]: [any, any] = useState('Kumar');
-  const [designation, setDesignation]: [any, any] = useState('Manager');
-  const [mobileNumber, setMobileNumber]: [any, any] = useState('8899832389');
-  const [idNumber, setIdNumber]: [any, any] = useState('165500');
-  const [location, setLocation]: [any, any] = useState('Delhi');
-  const [imagePath, setImagePath]: [any, any] = useState(null);
+  const [applicantName, setApplicantName]: [any, any] = useState('Raju Kumar');
+  const [totalAmount, setTotalAmount]: [any, any] = useState(1000);
+
+  const [paidAmount, setPaidAmount]: [any, any] = useState(300);
+  const [amountInWords, setAmountInWords]: [any, any] = useState(
+    'One Thousand Rupees'
+  );
+  const [caseFile, setCaseFile]: [any, any] = useState('5155');
+  const [paymentMode, setPaymentMode]: [any, any] = useState('UPI');
+  const [invoiceId, setInvoiceId]: [any, any] = useState('1677');
+  const [balanceAmount, setBalanceAmount]: [any, any] = useState('700');
 
   const onlyAlphabets = (e: any) => {
-    if (!/[a-z,A-Z]/.test(e.key)) {
+    if (!/[a-z,\s,A-Z]/.test(e.key)) {
       e.preventDefault();
     }
   };
 
-  const handleDownloadPdf = async () => {
-    const element: any = printRef.current;
-    const canvas = await html2canvas(element);
-    const data = canvas.toDataURL('image/png');
+  useEffect(() => {
+    setBalanceAmount(totalAmount - paidAmount);
+  }, [paidAmount]);
 
-    // eslint-disable-next-line new-cap
-    const pdf = new jsPDF({
-      orientation: 'portrait',
-      unit: 'in',
-      format: [10, 4],
-    });
-    const imgProperties = pdf.getImageProperties(data);
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
-
-    pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
-    pdf.save('print.pdf');
+  const onlyInteger = (e: any) => {
+    if (!/[0-9]/.test(e.key)) {
+      e.preventDefault();
+    }
   };
-  const router = useRouter();
+
+  function addWaterMark(doc: any) {
+    const img = new Image();
+    img.src = `${router.basePath}/assets/images/logo_op.png`;
+
+    doc.setTextColor(150);
+    doc.addImage(img, 'png', 30, 550, 660, 152, 'watermark', 'NONE', 20);
+
+    return doc;
+  }
+  const handleDownloadPdf = () => {
+    // eslint-disable-next-line new-cap
+    const doc = new jsPDF({
+      format: 'a3',
+      unit: 'px',
+      putOnlyUsedFonts: true,
+      floatPrecision: 16,
+    });
+
+    doc.html(printRef.current, {
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      margin: [160, 0, 20, 0],
+      autoPaging: 'text',
+
+      async callback(pdf) {
+        // save the document as a PDF with name of Memes
+        const totalPages = pdf.internal.pages.length - 1;
+        const img = new Image();
+        img.src = `${router.basePath}/assets/images/header.png`;
+
+        // eslint-disable-next-line no-plusplus
+        for (let i = 1; i <= totalPages; i++) {
+          pdf.setPage(i);
+
+          pdf.setFontSize(10);
+          // pdf.setTextColor(150);
+          pdf.text(
+            `Page ${i} of ${totalPages}`,
+            pdf.internal.pageSize.getWidth() - 100,
+            pdf.internal.pageSize.getHeight() - 30
+          );
+
+          pdf.addImage(img, 'png', 40, 0, 554, 182);
+          addWaterMark(pdf);
+        }
+        pdf.save('welcomeLetter');
+      },
+    });
+  };
+
+  // const router = useRouter();
   return (
     <Main>
       <section>
         <div className="mx-auto max-w-screen-2xl p-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
+          <div className="grid grid-cols-1 lg:grid-cols-2">
             <div className="relative flex items-center bg-gray-100">
               <span className="hidden lg:absolute lg:inset-y-0 lg:-left-16 lg:block lg:w-16 lg:bg-gray-100"></span>
 
               <div className=" py-4 px-8 sm:p-16 lg:p-8">
                 <h2 className="text-2xl font-bold sm:text-3xl text-indigo-500">
-                  Create Id Card
+                  Payment Invoice
                 </h2>
 
                 <form action="#" className="mt-4 grid grid-cols-6 gap-6">
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="firstName"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      First Name
-                    </label>
-
-                    <input
-                      type="text"
-                      id="firstName"
-                      name="firstName"
-                      onChange={(e) => setFirstName(e.target.value)}
-                      onKeyPress={onlyAlphabets}
-                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="lastName"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Last Name
-                    </label>
-
-                    <input
-                      type="text"
-                      id="lastName"
-                      name="lastName"
-                      onChange={(e) => setLastName(e.target.value)}
-                      onKeyPress={onlyAlphabets}
-                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-3">
-                    <label className="block text-sm font-medium text-gray-700">
-                      Upload profile pic (file)
-                    </label>
-                    <input
-                      type="file"
-                      className="mt-1 block w-full text-sm border-gray-500"
-                      onChange={(e) => {
-                        if (e.target.files) {
-                          setImagePath(e.target.files[0]);
-                        }
-                      }}
-                    />
-                  </div>
-
                   <div className="col-span-6 ">
                     <label
-                      htmlFor="designation"
+                      htmlFor="invoiceId"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      Designation
+                      Invoice Id (Required){' '}
                     </label>
 
                     <input
                       type="text"
-                      id="designation"
-                      name="designation"
-                      onChange={(e) => setDesignation(e.target.value)}
+                      id="invoiceId"
+                      name="invoiceId"
+                      onChange={(e) => setInvoiceId(e.target.value)}
+                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm"
+                    />
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="applicantName"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Bill To (Required){' '}
+                    </label>
+
+                    <input
+                      type="text"
+                      id="applicantName"
+                      name="applicantName"
+                      onChange={(e) => setApplicantName(e.target.value)}
+                      onKeyPress={onlyAlphabets}
+                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm"
+                    />
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="totalAmount"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Total Amount (Required)
+                    </label>
+
+                    <input
+                      type="text"
+                      id="totalAmount"
+                      name="totalAmount"
+                      onChange={(e) => setTotalAmount(e.target.value)}
+                      onKeyPress={onlyInteger}
+                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6">
+                    <label
+                      htmlFor="paidAmount"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Paid Amount (Required)
+                    </label>
+
+                    <input
+                      type="text"
+                      id="paidAmount"
+                      name="paidAmount"
+                      onChange={(e) => setPaidAmount(e.target.value)}
+                      onKeyPress={onlyInteger}
+                      maxLength={10}
+                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6">
+                    <label
+                      htmlFor="amountInWords"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Amount in Words
+                    </label>
+
+                    <input
+                      type="text"
+                      id="amountInWords"
+                      name="amountInWords"
+                      onKeyPress={onlyAlphabets}
+                      onChange={(e) => setAmountInWords(e.target.value)}
+                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm capitalize"
+                    />
+                  </div>
+                  <div className="col-span-6 sm:col-span-3 ">
+                    <label
+                      htmlFor="caseFile"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Case File No
+                    </label>
+
+                    <input
+                      type="text"
+                      id="caseFile"
+                      name="caseFile"
+                      onChange={(e) => setCaseFile(e.target.value)}
                       className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm capitalize"
                     />
                   </div>
 
                   <div className="col-span-6 sm:col-span-3">
                     <label
-                      htmlFor="mobileNumber"
+                      htmlFor="paymentMode"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      Contact Number
+                      Payment Mode (Required)
                     </label>
 
                     <input
                       type="text"
-                      id="mobileNumber"
-                      name="mobileNumber"
-                      onChange={(e) => setMobileNumber(e.target.value)}
-                      onKeyPress={(event) => {
-                        if (!/[0-9]/.test(event.key)) {
-                          event.preventDefault();
-                        }
-                      }}
-                      maxLength={10}
-                      className="mt-1 w-full rounded-md border-gray-500 bg-white text-sm text-gray-700 shadow-sm"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="idNumber"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Id Number
-                    </label>
-
-                    <input
-                      type="text"
-                      id="idNumber"
-                      name="idNumber"
-                      onChange={(e) => setIdNumber(e.target.value)}
+                      id="paymentMode"
+                      name="paymentMode"
+                      onChange={(e) => setPaymentMode(e.target.value)}
                       className="mt-1 w-full rounded-md border-gray-500  text-sm text-gray-700 shadow-sm  bg-white
                         "
                     />
                   </div>
-                  <div className="col-span-6">
-                    <label
-                      htmlFor="location"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Location
-                    </label>
 
-                    <input
-                      type="text"
-                      id="location"
-                      name="location"
-                      onChange={(e) => setLocation(e.target.value)}
-                      className="mt-1 w-full rounded-md border-gray-500 bg-white
-                        text-sm text-gray-700 shadow-sm"
-                    />
-                  </div>
                   <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                     <button
                       onClick={(e) => {
@@ -193,56 +239,17 @@ const Contact = () => {
             </div>
             <div className="relative z-10 lg:py-16">
               <div className="flex-col items-center relative flex">
-                <div
-                  ref={printRef}
-                  className="h-auto flex flex-col justify-center items-center  border-2 border-gray-600 w-80 p-4 rounded-md"
-                >
-                  <div className="h-1/5 relative border-b border-gray-500 m-3 pb-3">
-                    <img
-                      src={`${router.basePath}/assets/images/logo2.png`}
-                      alt=""
-                    />
-                  </div>
-                  <div className="h-2/5  relative mt-2">
-                    {imagePath && (
-                      <img
-                        alt="not found"
-                        src={URL.createObjectURL(imagePath)}
-                        className="rounded-full w-44 h-44 border-2 border-gray-600 p-1"
-                      />
-                    )}
-                  </div>
-                  <div className="h-1/5 relative my-4 font-bold">
-                    {firstName} {lastName}{' '}
-                  </div>
-                  <div className="h-auto relative font-semibold text-base my-2">
-                    <div className="">
-                      <span className="inline-block w-40">Designation : </span>
-                      {designation}
-                    </div>
-                    <div className="">
-                      <span className="inline-block w-40">
-                        Mobile Number :{' '}
-                      </span>
-                      {mobileNumber}
-                    </div>
-                    <div className="">
-                      <span className="inline-block w-40">Id Number : </span>
-                      {idNumber}
-                    </div>
-                    <div className="">
-                      <span className="inline-block w-40">Location : </span>
-                      {location}
-                    </div>
-                  </div>
-                  <div className="relative text-sm">
-                    www.seveno.in
-                    <img
-                      alt="stamp"
-                      src={`${router.basePath}/assets/images/stamp.png`}
-                      className="w-16 h-16 absolute -right-20 -top-9"
-                    />
-                  </div>
+                <div ref={printRef} className="p-0 m-0">
+                  <PayslipTemplate
+                    applicantName={applicantName}
+                    totalAmount={totalAmount}
+                    paidAmount={paidAmount}
+                    amountInWords={amountInWords}
+                    caseFile={caseFile}
+                    paymentMode={paymentMode}
+                    invoiceId={invoiceId}
+                    balanceAmount={balanceAmount}
+                  ></PayslipTemplate>
                 </div>
                 <div className="col-span-6 sm:flex sm:items-center sm:gap-4 mt-8">
                   <button
